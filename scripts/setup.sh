1fd1af075578c50431438247c19dee2d5f697c89
Overycall <lalabarcellos0@gmail.com>
2023-05-22 19:53:47 -0300

    Teste_1 150 APKs

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install curl
+sudo apt install time
+sudo apt install python3.8
+sudo apt install python3-pip
+sudo apt install wget
+sudo python3 -m pip install androguard==3.3.5
+sudo python3 -m pip install networkx==2.2
+sudo python3 -m pip install pandas==1.3.5
+sudo python3 -m pip install termcolor==1.1.0
+sudo python3 -m pip install pyfiglet==0.8.post1
+sudo python3 -m pip install requests==2.22.0
---
+++ tests/README.md
@@ -0,0 +1,3 @@
+# Testes realizados na ADBuilder
+
+Link da planilha das métricas da ferramenta: https://docs.google.com/spreadsheets/d/1kcjz8hwdXwyWQBqUe1zTfAR0GmaEpSFaH-ULk-jZKzw/edit?usp=sharing
